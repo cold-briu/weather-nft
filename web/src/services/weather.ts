a250d77dedddf1658b9a053a2f9732622c776b17
Andrés Fernández <andresavena19@gmail.com>
2023-02-01 05:34:27 -0500

    added weather service

--- web/src/services/weather.ts
+++ web/src/services/weather.ts
@@ -1,10 +1,27 @@
+import Axios from "axios";
+import { isConditionalExpression } from "typescript";
+import { config } from "../config";
+
+const api = Axios.create({
+	baseURL: config.weather.baseUrl,
+	params: {
+		KEY: config.weather.key
+	}
+})
+
 type params = {
 	latitude: number;
 	longitude: number;
 }
 export const getWeather = async (location: params) => {
 	const { latitude, longitude } = location;
-	const queryParam = `q=${latitude},${longitude}`
+	let res = await api.get(`/current`, {
+		params: {
+			q: `${latitude},${longitude}`
+		}
+	})
+	console.log(res);
+
 	// TODO return type
-	return { location }
+	return res.data.condition
 }
