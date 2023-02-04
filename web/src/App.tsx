5c5ebdc30f9d8b9b4c0cda0998640110178bde42
Andrés Fernández <andresavena19@gmail.com>
2023-02-03 23:41:23 -0500

    feat: added rpc info

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -18,8 +18,15 @@ function App() {
             <div className="d-flex flex-column">
               <h1 className="display-6 fw-bolder" >🌥 weather nft 🌥</h1>
               <h6 className="lead">🐶  by sandusky.eth ❤️</h6>
-              <p>This was deployed on the ArbitrumGoerli tesnet.</p>
-              <p>Remember to press the image to mint & save.</p>
+              <p>This was deployed on the ArbitrumGoerli tesnet. <br />
+                Remember to press over the image to mint & save.</p>
+              <p className="small">
+                connection info:
+                <br />
+                rpc: https://arb-goerli.g.alchemy.com/v2/pwZBBwuLBDEuhmkqJqw32Bgza8cloMxc
+                <br />
+                chainid: 421613
+              </p>
               <div className="d-flex flex-row justify-content-evenly">
                 <a href="https://faucet.triangleplatform.com/arbitrum/goerli" target="_blank" className="btn btn-outline-secondary btn-sm">
                   Facucet
